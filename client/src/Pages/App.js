5b2b16bf264489c981d15662ef945b4546d71071
Megan <dommelmeg@gmail.com>
2023-05-30 01:22:32 -0400

    installed react router

--- client/src/Pages/App.js
+++ client/src/Pages/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Grid, Text, Box, HStack, Flex } from '@chakra-ui/react'
+import { Grid, Text, Box, HStack, Flex, ChakraProvider } from '@chakra-ui/react'
 import NavDrawer from '../Components/NavDrawer';
 import Dashboard from './Dashboard';
+import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <Flex>
-      <Box h='calc(100vh)' w='xs' boxShadow='lg' position='fixed'>
-        <NavDrawer />
-      </Box>
+    <ChakraProvider>
 
-      <Box w='full' ml='320px'>
-        <Dashboard />
-      </Box>
-    </Flex>
+      <Flex>
+        <Box h='calc(100vh)' w='xs' boxShadow='lg' position='fixed'>
+          <NavDrawer />
+        </Box>
+
+        <Box w='full' ml='320px'>
+          <Dashboard />
+        </Box>
+      </Flex>
+      
+    </ChakraProvider>
   );
 }
 
