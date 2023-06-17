16d67abfc4cf5d0c016ea8c9b81afbd5858a01a5
Megan <dommelmeg@gmail.com>
2023-06-16 23:33:24 -0400

    fixes header justification

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,17 +1,19 @@
 import React, { useContext } from "react";
-import { Box, Switch, AbsoluteCenter, Avatar, useRadio } from "@chakra-ui/react";
+import { Box, Switch, AbsoluteCenter, Avatar, useRadio, Text, HStack, Flex } from "@chakra-ui/react";
 import { VendorBookingContext } from "../context/vendorBooking";
 
 const Header = () => {
   const { user } = useContext(VendorBookingContext)
 
   return (
-    <Box h='20' bg='white' boxShadow='lg' position='sticky' >
-      <AbsoluteCenter axis='vertical' p='2'>
-        {/* <Switch /> */}
-        <Avatar name={user.username} />
+    <Flex h='20' bg='white' boxShadow='md' position='sticky' justify='right'>
+      <AbsoluteCenter axis='vertical' p='2' >
+        <HStack padding='4'>
+          <Text>Welcome, <b>{user.username}</b>!</Text>
+          <Avatar name={user.username} />
+        </HStack>
       </AbsoluteCenter>
-    </Box>
+    </Flex>
   )
 }
 
